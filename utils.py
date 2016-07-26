2fb1e4e7c8dc21f29280717d75348b79dbc69af9
boully <sweeliao@163.com>
2016-07-26 22:36:48 +0800

    add -F to enforce git reset to master

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 #encoding=utf-8
-
+import os
 import sys
 
 
@@ -17,9 +17,9 @@ def GreenIt(s):
         return s
 
 
-def PullRepo(repo):
+def PullRepo(repo, path):
     # may have another way to do this
     repo.head.ref = repo.heads.master
     repo.head.reset(index=True, working_tree=True)
-    res = repo.git.reset('--hard','origin/master')
-    print GreenIt(res)
+    repo.git.reset('--hard','origin/master')
+    print GreenIt("{0} [{1}] {2} set success.".format(path.split(os.path.sep)[-1], repo.head.ref, path))
