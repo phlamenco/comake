5db58ab189fa47f925a1a7cf4aec48c9e76d31a3
boully <sweeliao@163.com>
2016-08-25 00:19:15 +0800

    change project into a pipy one

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+      name = 'comake',
+      packages = ['comake'], # this must be the same as the name above
+      version = 'v0.1.0',
+      description = 'A c++ build tool',
+      author = 'liaosiwei',
+      author_email = 'liaosiwei@163.com',
+      url = 'https://github.com/boully/comake', # use the URL to the github repo
+      download_url = 'https://github.com/boully/comake/tarball/v0.1.0', # I'll explain this in a second
+      keywords = ['c++', 'auto-build', 'dependency'], # arbitrary keywords
+      classifiers = [],
+    )
+)
