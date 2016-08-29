c8f22ac8f453320944330c8bc37d621636bbb473
boully <sweeliao@163.com>
2016-08-30 00:23:47 +0800

    add find_packages

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-    setup(
+from setuptools import setup, find_packages
+
+setup(
     name = 'comake',
-    packages = ['comake'], # this must be the same as the name above
-    version = 'v0.1.1',
+    packages=find_packages(), # this must be the same as the name above
+    version = 'v0.1.2',
     description = 'A c++ build tool',
     author = 'liaosiwei',
     author_email = 'liaosiwei@163.com',
-    url = 'https://github.com/boully/comake', # use the URL to the github repo
-    download_url = 'https://github.com/boully/comake/tarball/v0.1.1', # I'll explain this in a second
-    keywords = ['c++', 'auto-build', 'dependency'], # arbitrary keywords
-    classifiers = [],
+    url = 'https://github.com/boully/comake',
+    download_url = 'https://github.com/boully/comake/tarball/v0.1.2',
+    keywords = ['c++', 'auto-build', 'dependency'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
     install_requires=[
         "GitPython",
         "pytoml",
         "Jinja2",
     ],
+    entry_points={
+        'console_scripts': [
+            'comake=comake:main',
+        ],
+    },
 )
